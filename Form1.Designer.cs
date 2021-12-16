779b0f15ccd751507b0b39d9ff3925d057484825
itsmerifz <64636917+muhammadarief-17@users.noreply.github.com>
2021-12-16 22:44:11 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ClientReservasi_20190140015
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.IDReservasi = new System.Windows.Forms.TextBox();
+            this.Nama = new System.Windows.Forms.TextBox();
+            this.NoTelepon = new System.Windows.Forms.TextBox();
+            this.JumlahPemesanan = new System.Windows.Forms.TextBox();
+            this.IDLokasi = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataPemesanan = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataPemesanan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Reservasi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "No Telepon";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Jumlah Pemesanan";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ID Lokasi";
+            // 
+            // IDReservasi
+            // 
+            this.IDReservasi.Location = new System.Drawing.Point(160, 12);
+            this.IDReservasi.Name = "IDReservasi";
+            this.IDReservasi.Size = new System.Drawing.Size(245, 20);
+            this.IDReservasi.TabIndex = 5;
+            // 
+            // Nama
+            // 
+            this.Nama.Location = new System.Drawing.Point(160, 55);
+            this.Nama.Name = "Nama";
+            this.Nama.Size = new System.Drawing.Size(245, 20);
+            this.Nama.TabIndex = 6;
+            // 
+            // NoTelepon
+            // 
+            this.NoTelepon.Location = new System.Drawing.Point(160, 99);
+            this.NoTelepon.Name = "NoTelepon";
+            this.NoTelepon.Size = new System.Drawing.Size(245, 20);
+            this.NoTelepon.TabIndex = 7;
+            // 
+            // JumlahPemesanan
+            // 
+            this.JumlahPemesanan.Location = new System.Drawing.Point(160, 143);
+            this.JumlahPemesanan.Name = "JumlahPemesanan";
+            this.JumlahPemesanan.Size = new System.Drawing.Size(245, 20);
+            this.JumlahPemesanan.TabIndex = 8;
+            // 
+            // IDLokasi
+            // 
+            this.IDLokasi.Location = new System.Drawing.Point(160, 190);
+            this.IDLokasi.Name = "IDLokasi";
+            this.IDLokasi.Size = new System.Drawing.Size(245, 20);
+            this.IDLokasi.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(519, 24);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Simpan";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(519, 74);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(519, 128);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Hapus";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(519, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataPemesanan
+            // 
+            this.dataPemesanan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataPemesanan.Location = new System.Drawing.Point(16, 247);
+            this.dataPemesanan.Name = "dataPemesanan";
+            this.dataPemesanan.Size = new System.Drawing.Size(673, 168);
+            this.dataPemesanan.TabIndex = 14;
+            this.dataPemesanan.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataPemesanan_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 427);
+            this.Controls.Add(this.dataPemesanan);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.IDLokasi);
+            this.Controls.Add(this.JumlahPemesanan);
+            this.Controls.Add(this.NoTelepon);
+            this.Controls.Add(this.Nama);
+            this.Controls.Add(this.IDReservasi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataPemesanan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox IDReservasi;
+        private System.Windows.Forms.TextBox Nama;
+        private System.Windows.Forms.TextBox NoTelepon;
+        private System.Windows.Forms.TextBox JumlahPemesanan;
+        private System.Windows.Forms.TextBox IDLokasi;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataPemesanan;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientReservasi_20190140015
+{
+    public partial class Form1 : Form
+    {
+        ServiceReference1.Service1Client service = new ServiceReference1.Service1Client();
+        public Form1()
+        {
+            InitializeComponent();
+
+            TampilData();
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string idPemesanan = IDReservasi.Text;
+            string namaCust = Nama.Text;
+            string noTelp = NoTelepon.Text;
+            int jmlPemesanan = int.Parse(JumlahPemesanan.Text);
+            string idLokasi = IDLokasi.Text;
+
+            var a = service.pemesanan(idPemesanan,namaCust,noTelp,jmlPemesanan,idLokasi);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string idPemesanan = IDReservasi.Text;
+            string namaCust = Nama.Text;
+            string noTelp = NoTelepon.Text;
+
+            var a = service.editPemesanan(idPemesanan, namaCust,noTelp);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+        }
+
+        public void TampilData()
+        {
+            var list = service.Pemesanan1();
+            dataPemesanan.DataSource = list;
+        }
+
+        public void Clear()
+        {
+            IDReservasi.Clear();
+            Nama.Clear();
+            NoTelepon.Clear();
+            IDLokasi.Clear();
+            JumlahPemesanan.Clear();
+
+            JumlahPemesanan.Enabled = true;
+            IDLokasi.Enabled = true;
+
+            btnSave.Enabled = true;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+
+            IDReservasi.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void dataPemesanan_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            IDReservasi.Text = Convert.ToString(dataPemesanan.Rows[e.RowIndex].Cells["idPemesanan"].Value);
+            Nama.Text = Convert.ToString(dataPemesanan.Rows[e.RowIndex].Cells["namaCustomer"].Value);
+            NoTelepon.Text = Convert.ToString(dataPemesanan.Rows[e.RowIndex].Cells["noTelepon"].Value);
+            JumlahPemesanan.Text = Convert.ToString(dataPemesanan.Rows[e.RowIndex].Cells["jumlahPemesanan"].Value);
+            IDLokasi.Text = Convert.ToString(dataPemesanan.Rows[e.RowIndex].Cells["Lokasi"].Value);
+
+            JumlahPemesanan.Enabled = false;
+            IDLokasi.Enabled = false;
+
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+
+            btnSave.Enabled = false;
+            IDReservasi.Enabled = false;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string idPemesanan = IDReservasi.Text;
+
+            var a = service.deletePemesanan(idPemesanan);
+            MessageBox.Show(a);
+            TampilData();
+            Clear();
+        }
+    }
+}
